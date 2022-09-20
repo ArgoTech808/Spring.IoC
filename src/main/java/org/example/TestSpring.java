1f7996ab2d869164ecc5e060370ab87bea538f14
NOTuyoung <foks12357@mail.ru>
2022-09-20 15:59:17 +0300

    Initial commit

---
+++ src/main/java/org/example/TestSpring.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        Music music = context.getBean("testMusicBean", Music.class);
+
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+
+        musicPlayer.playMusic();
+
+        context.close();
+
+
+    }
+
+}
